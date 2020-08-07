a2ed79462dff9ecf747dd203d7d03bdb9e86fa9a
Dmitriy Porozov <kironitygaming@gmail.com>
2020-08-07 15:08:13 +0300

    Refactoring

---
+++ Source/Telekenesis/Public/TelekenesisHUD.h
@@ -0,0 +1,20 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TelekenesisHUD.generated.h"
+
+UCLASS()
+class ATelekinesisHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ATelekinesisHUD();
+
+private:
+
+};
+
--- Source/Telekenesis/Telekenesis.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "Telekenesis.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Telekenesis, "Telekenesis" );
- 
